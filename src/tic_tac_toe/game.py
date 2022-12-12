889e83b158b98c3e5739139d7c1ff88378a6c895
PrateekKhindri <12605@coderacademy.edu.au>
2022-12-12 19:23:06 +1100

    T1A3-004 - Displayed welcome screen

--- src/tic_tac_toe/game.py
+++ src/tic_tac_toe/game.py
@@ -1,16 +1,28 @@
-from constants import style
+from constants import style, game_mode_options, title
 from prompt_toolkit.shortcuts import yes_no_dialog
+from lib.display import Display
 
 
 class Game:
     def __init__(self):
-        pass
+        self.game_mode_choice = None
 
     def start(self):
-        self.welcome_screen()
+        start_game = self.welcome_screen()
+        if start_game:
+            self.game_mode_choice = self.select_game_mode()
 
     def welcome_screen(self):
         return yes_no_dialog(
             title='Welcome to Tic Tac Toe ',
             text='Created by Prateek Khindri\n\nWould you like to play a game?', style=style
         ).run()
+
+    def select_game_mode(self):
+        Display.show_game_mode_select_screen()
+
+        user_option = Display.rich_input(
+            "\n\tSelect a mode from the options above", game_mode_options
+        )
+
+        return int(user_option)
---
+++ src/tic_tac_toe/lib/display.py
@@ -0,0 +1,34 @@
+import termcolor
+import rich
+from rich.table import Table
+from rich.padding import Padding
+from rich.prompt import Prompt
+from constants import console, text_color
+
+
+class Display:
+    @classmethod
+    def rich_input(self, text, options):
+        text = rich.text.Text(text, style=text_color.get('input_text'))
+        return Prompt.ask(text, choices=options)
+
+    @classmethod
+    def show_game_mode_select_screen(self):
+        print("\t---------------------------------------------------")
+        termcolor.cprint(
+            "\t|        Welcome To Unbeatable Tictactoe          |", color="white", attrs=["bold"])
+        print("\t---------------------------------------------------")
+        print("\t              Developed with 💜 by ", end=" ")
+        termcolor.cprint("\n\n                        Prateek Khindri",
+                         color="green", attrs=["bold"])
+        print("\t---------------------------------------------------\n")
+
+        table = Table(title="Select Game Mode",
+                      title_justify="center", width=50)
+        table.add_column("S.No", style="cyan", no_wrap=True)
+        table.add_column("Game Mode", style="magenta")
+
+        table.add_row("1", "Player Vs Computer")
+        table.add_row("2", "MultiPlayer")
+
+        console.print(Padding(table, pad=(2, 0, 1, 8)))
