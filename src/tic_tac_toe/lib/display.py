ac04f7f37e20e76dca73ea16cea0a4f95d97cf79
PrateekKhindri <12605@coderacademy.edu.au>
2022-12-13 11:49:28 +1100

    T1A3-004 - Added static class methods show_select_symbol_screen and fancy_print

--- src/tic_tac_toe/lib/display.py
+++ src/tic_tac_toe/lib/display.py
@@ -32,3 +32,21 @@ class Display:
         table.add_row("2", "MultiPlayer")
 
         console.print(Padding(table, pad=(2, 0, 1, 8)))
+
+    @classmethod
+    def show_select_symbol_screen():
+        table = Table(title="Select Symbol", title_justify="center", width=50)
+        table.add_column("S.No", style="cyan", no_wrap=True)
+        table.add_column("Symbol", style="magenta")
+
+        table.add_row("1", "X")
+        table.add_row("2", "O")
+
+        console.print(Padding(table, pad=(2, 0, 1, 8)))
+
+    @classmethod
+    def fancy_print(self, text):
+        print("\t---------------------------------------------------")
+        termcolor.cprint(
+            f"\t|                {text}              |", color="white")
+        print("\t---------------------------------------------------")
