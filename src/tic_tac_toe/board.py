c7ffa07d809fb1ef44064351adfc288140ce1ef6
PrateekKhindri <12605@coderacademy.edu.au>
2022-12-13 12:22:21 +1100

    T1A3-004 - Added Board and Player classes

---
+++ src/tic_tac_toe/board.py
@@ -0,0 +1,12 @@
+import termcolor
+import emoji
+from lib.display import Display
+from constants import *
+from rich.prompt import Prompt
+from rich.text import Text
+from player import Player
+
+
+class Board:
+    def __init__(self):
+        pass
---
+++ src/tic_tac_toe/player.py
@@ -0,0 +1,5 @@
+class Player:
+    def __init__(self):
+        self.X = None
+        self.O = None
+        self.name = None
